5da5c72f726c49770f2fd3e2079da6fe16fad36d
Anshita <anshita_b@yahoo.co.in>
2020-04-23 18:49:20 +0530

    heroku

--- quiccar/quiccar/settings.py
+++ quiccar/quiccar/settings.py
@@ -20,7 +20,7 @@ print( BASE_DIR)
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config ('SECRET_KEY')
+SECRET_KEY = os.environ.get ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +84,7 @@ DATABASES = {
 '''
 DATABASES = {
 	'default':dj_database_url.config (
-		default = config ('DATABASE_URL')
+		default = os.environ.get ('DATABASE_URL')
 	)
 }
 
