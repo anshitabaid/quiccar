b81c83be9fa47a754d66bbe7ba6aff34a8f0e989
Anshita <anshita_b@yahoo.co.in>
2020-04-23 19:29:09 +0530

    heroku

--- quiccar/quiccar/settings.py
+++ quiccar/quiccar/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'quiccar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -91,8 +91,8 @@ DATABASES = {
     )
 }
 '''
-prod_db  =  dj_database_url.config()
-DATABASES['default'].update(prod_db)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
