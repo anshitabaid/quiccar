bca9477474dc14897bfc9b880071cfab4c5e741f
Anshita <anshita_b@yahoo.co.in>
2020-04-23 18:56:53 +0530

    heroku

--- quiccar/quiccar/settings.py
+++ quiccar/quiccar/settings.py
@@ -84,11 +84,10 @@ DATABASES = {
 }
 '''
 DATABASES = {
-	'default':dj_database_url.config (
-		default = os.environ.get ('DATABASE_URL')
-	)
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
