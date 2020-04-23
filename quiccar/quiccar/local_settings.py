9256cb213b2912d59873f1f6bab2f687f9c7a889
Anshita <anshita_b@yahoo.co.in>
2020-04-23 21:25:07 +0530

    mod error

--- quiccar/quiccar/local_settings.py
+++ quiccar/quiccar/local_settings.py
@@ -1,5 +1,5 @@
 import os
-from .secrets import *
+#from .secrets import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
